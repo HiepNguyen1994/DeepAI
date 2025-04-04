410a943faec9a9f294a49637644a34a8b421079a
HiepNguyen1994 <hiep.buh@gmail.com>
2025-04-04 15:46:04 +0700

    query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -7,7 +7,8 @@ from collections import defaultdict
 import streamlit as st
 
 
-openai.api_key = st.secrets["openai_key"]
+openai.api_key = st.secrets["api"]["openai_key"]
+
 
 
 # query_engine.py - Đã fix fallback BM25 + FAISS + lọc thời gian
