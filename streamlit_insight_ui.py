54a4df6004a9abc23942f316b329f485a12327dd
HiepNguyen1994 <hiep.buh@gmail.com>
2025-04-04 14:02:29 +0700

    streamlit_insight_ui.py

--- streamlit_insight_ui.py
+++ streamlit_insight_ui.py
@@ -17,6 +17,7 @@ import gdown
 st.set_page_config(layout="centered")
 
 # Danh sách các file FAISS cần tải nếu chưa có
+
 file_links = {
     "faiss_banking_index.bin": "https://drive.google.com/uc?id=1lIFGw_O3pTsVE5MQx0sEqWX8MMiz00ei",
     "faiss_key_map.json": "https://drive.google.com/uc?id=1r--suOBbJ2ROIcy6lYlVworfDx--k-S2",
